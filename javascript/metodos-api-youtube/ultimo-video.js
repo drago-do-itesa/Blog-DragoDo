a5ea3bb3becc8157d7e8acd0180cd2adea0baaaa
Drago <18030536@itesa.edu.mx>
2021-11-02 15:09:13 -0600

    Terminamos de implementar el eleemto que muestra en la pagina principal el ultimo video subido al canal (esto haciendo uso de una api de YOUTUBE)... Aun asi nos falta darle estilo a nuestro elemento mediante CSS

--- javascript/metodos-api-youtube/ultimo-video.js
+++ javascript/metodos-api-youtube/ultimo-video.js
@@ -23,60 +23,22 @@ function lastVideoData() {
   xhttp.onreadystatechange = function (params) {
     if (this.readyState == 4 && this.status == 200) {
       //Mostramos el texto una vez obtenido
-      //console.log(this.responseText);
+      //*console.log(this.responseText);
       //Creacion de JSON para mejor manejo de datos
       let datos = JSON.parse(this.responseText);
-      console.log(datos.items[0].id.videoId);
+      //* console.log(datos.items[0].id.videoId);
+      //Una vez que creamos el JSON, ahora extraemos los datos de este para guardarlos en variables.
       let titulo = datos.items[0].snippet.title;
       let descripcion = datos.items[0].snippet.description;
       let url_img = datos.items[0].snippet.thumbnails.high.url;
-      //!! --------------------------------------------Aqui me quedeee */
       let url_video =
         "https://www.youtube.com/watch?v=" + datos.items[0].id.videoId;
+
+      //Ahora utilizamos estas variables para modificar el DOM y que se actualizen los datos de manera automatica de acuerdo al ultimo video subido
+      document.getElementById("video-title").textContent = titulo;
+      document.getElementById("video-description").textContent = descripcion;
+      document.getElementById("video-img").setAttribute("src", url_img);
+      document.getElementById("video-url").setAttribute("href", url_video);
     }
   };
-
-  console.log("correcto final");
-}
-
-function lastVideoDataBorrador() {
-  //Arraylist con los datos del video
-  let datosVideo = [];
-  var nextPageToken = "";
-  // Resultados por pagina
-  var resPorPagina = 1;
-  // Paginas a mostrar
-  var paginas = 1;
-  //Clave de la api de mi canal
-  var key = "AIzaSyBzAO69uRpQOZHoGjMnC5uiQnDlWaXAEVU";
-  //Id de mi canal
-  var idCanal = "UCBcxQfwjGiflQdiL5wYcZOQ";
-  //Url que llamara a la API y regresa un JSON
-  var url =
-    "https://www.googleapis.com/youtube/v3/search?key=" +
-    key +
-    "&channelId=" +
-    idCanal +
-    "&part=snippet,id&order=date&maxResults=" +
-    resPorPagina;
-
-  $("#show-last-video").append(url);
-  $.getJSON(url, function (data) {
-    for (var k in data.items) {
-      //Agrega el titulo al array
-      datosVideo.push(data.items[k]["snippet"].title);
-      //Agrega el url al array
-      datosVideo.push(
-        "https://www.youtube.com/watch?v=" + data.items[k]["id"].videoId
-      );
-      //Agrega el descripcion al array
-      datosVideo.push(data.items[k]["snippet"].description);
-      //Agrega el url-IMG al array
-      datosVideo.push(data.items[k]["snippet"].thumbnails.high.url);
-
-      for (let index = 0; index < datosVideo.length; index++) {
-        console.log(datosVideo[index]);
-      }
-    }
-  });
 }
