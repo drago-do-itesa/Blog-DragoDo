2ecb270a762917b46a9dd057aa065044452a8038
Drago Windows <18030536@itesa.edu.mx>
2021-11-06 11:48:47 -0600

    Agregada la logica de la grilla de la parte final. Se corrigio la forma en la que se llaman los blogs. La direccion de estos seran los ultimos caracteres de la de el video en youtube, ya que al igual que en youtube esta ono cambiará.

--- javascript/metodos-api-youtube/ultimo-video.js
+++ javascript/metodos-api-youtube/ultimo-video.js
@@ -2,7 +2,7 @@ function videoDataJSON() {
   // Resultados por pagina
   var resPorPagina = 50;
   // Paginas a mostrar
-  var paginas = 1;
+  var paginas = 50;
   //Clave de la api de mi canal
   var key = "AIzaSyBzAO69uRpQOZHoGjMnC5uiQnDlWaXAEVU";
   //Id de mi canal
@@ -16,7 +16,7 @@ function videoDataJSON() {
     "&part=snippet,id&order=date&maxResults=" +
     resPorPagina;
 
-  console.log(url);
+  // console.log(url);
   //Obtenemos el texto sin formato de la url
   const xhttp = new XMLHttpRequest();
   xhttp.open("GET", url, true);
@@ -29,14 +29,53 @@ function videoDataJSON() {
       let datos = JSON.parse(this.responseText);
       //* console.log(datos.items[0].id.videoId);
       //Una vez que creamos el JSON, ahora extraemos los datos de este para guardarlos en variables.
-      let title = datos.items[0].snippet.title;
-      let description = datos.items[0].snippet.description;
-      let url_img = datos.items[0].snippet.thumbnails.high.url;
-      let url_video =
-        "https://www.youtube.com/watch?v=" + datos.items[0].id.videoId;
-      lastVideoData(title, description, url_img, url_video);
+      let title = [];
+      let description = [];
+      let url_img = [];
+      let url_video = [];
+      let url_blog = [];
+
+      for (let index = 0; index < 8; index++) {
+        title.push(datos.items[index].snippet.title);
+        description.push(datos.items[index].snippet.description);
+        url_img.push(datos.items[index].snippet.thumbnails.high.url);
+        url_video.push(
+          "https://www.youtube.com/watch?v=" + datos.items[index].id.videoId
+        );
+        url_blog.push("/blogs/" + datos.items[index].id.videoId);
+      }
+
+      // metodos para rellenar cabecera y grid
+      lastVideoData(title[0], description[0], url_img[0], url_video[0]);
+
+      videosOnGrid(title, description, url_img, url_video, url_blog);
     }
   };
+}
+
+function videosOnGrid(title, description, url_img, url_video, url_blog) {
+  let identificador = 1;
+  for (let index = 0; index < 8; index++) {
+    document
+      .getElementById("video-cards-img-" + identificador)
+      .setAttribute("src", url_img[index]);
+
+    document.getElementById("video-cards-title-" + identificador).textContent =
+      title[index];
+
+    document.getElementById("video-cards-desc-" + identificador).textContent =
+      description[index];
+
+    document
+      .getElementById("video-cards-blog-" + identificador)
+      .setAttribute("href", url_blog[index]);
+
+    document
+      .getElementById("video-cards-vid-" + identificador)
+      .setAttribute("href", url_video[index]);
+
+    identificador++;
+  }
 }
 
 function lastVideoData(title, description, url_img, url_video) {
